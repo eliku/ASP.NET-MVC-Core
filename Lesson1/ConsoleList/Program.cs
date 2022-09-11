9eb66ab693606e4469cc6c37c9726220fba537d0
GeekBrains <kubasovae@mail.ru>
2022-09-11 22:26:18 +0400

    The end

---
+++ Lesson1/ConsoleList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clList<int> listOne = new clList<int>();
+            clList<int> listTwo = new clList<int>();
+
+            Thread thread1 = new(() => listOne.Add(1));
+            Thread thread2 = new(() => listOne.Add(5));
+            Thread thread3 = new(() => listTwo.Add(32));
+            Thread thread4 = new(() => listTwo.Add(34));
+            Thread thread5 = new(() => listOne.Remove(1));
+            Thread thread6 = new(() => listTwo.Remove(1));
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+            thread5.Start();
+            thread6.Start();
+
+            for (int i = 0; i < listOne._list.Count; i++)
+            {
+                Console.WriteLine($"listOne[{i}] = {listOne._list[i]}");
+            }
+            for (int i = 0; i < listTwo._list.Count; i++)
+            {
+                Console.WriteLine($"listTwo[{i}] = {listTwo._list[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson1/Lesson1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Lesson1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int tick = 0;
+        public DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();            
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Thread threadNew = new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = false;
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, (SendOrPostCallback)delegate
+                {
+                    tbFibonacci.Text = $"{tick}: {Convert.ToString(Fibonachi(tick))};\n";
+                }, null);
+            });
+
+            threadNew.Start();
+            tick++;
+        }
+
+        private int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ((Slider)sender).SelectionEnd = e.NewValue;
+            timer.Interval = TimeSpan.FromSeconds(e.NewValue);
+        }
+    }
+}
