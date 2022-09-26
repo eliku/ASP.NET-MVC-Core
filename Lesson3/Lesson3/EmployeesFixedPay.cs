e4e38fa4a2682faf991b860fb58db68981304cc0
GeekBrains <kubasovae@mail.ru>
2022-09-26 23:09:11 +0400

    task1

---
+++ Lesson3/Lesson3/EmployeesFixedPay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    public class EmployeesFixedPay : Employee
+    {
+        protected internal override double Calculation(double Sum)
+        {
+            return Sum;
+        }
+    }
+}
---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new();
+            Employee[] arrEmploy = new Employee[6];
+            arrEmploy[0] = new TimeWorker();
+            arrEmploy[0]._fullName = "Иванов А.В.";
+            arrEmploy[0]._salary = arrEmploy[0].Calculation(2000);
+
+            arrEmploy[1] = new EmployeesFixedPay();
+            arrEmploy[1]._fullName = "Иванова А.А.";
+            arrEmploy[1]._salary = arrEmploy[1].Calculation(25000);
+
+            arrEmploy[2] = new TimeWorker();
+            arrEmploy[2]._fullName = "Сидоров А.В.";
+            arrEmploy[2]._salary = arrEmploy[2].Calculation(1500);
+
+            arrEmploy[3] = new EmployeesFixedPay();
+            arrEmploy[3]._fullName = "Сидорова А.А.";
+            arrEmploy[3]._salary = arrEmploy[3].Calculation(70000);
+
+            arrEmploy[4] = new EmployeesFixedPay();
+            arrEmploy[4]._fullName = "Петров А.А.";
+            arrEmploy[4]._salary = arrEmploy[4].Calculation(50000);
+
+            for (int i = 0; i < arrEmploy.Length; i++)
+                Console.WriteLine($"{arrEmploy[i]._fullName} с зарплатой  {arrEmploy[i]._salary}");
+        }
+    }
+}
